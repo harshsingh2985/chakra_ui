a2eed897ebb8612096d633600d04f617f9000407
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2023-03-24 23:57:40 +0530

    update:

---
+++ my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route, Routes
+} from 'react-router-dom';
+
+
+function App(){
+  return(
+    <>
+    <Router>
+      <div>Hi this is harsh singh</div>
+
+
+      <Routes>
+
+      <Route>
+       
+      </Route>
+      </Routes>
+      
+    </Router>
+    </>
+  )
+}
+
+export default App;
